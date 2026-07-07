642232be05ec167a8c5dd6ef37b20e5380d38158
divyamarelly <divyamarelly21@gmail.com>
2026-07-07 19:07:33 +0530

    Modules in Python

---
+++ modules/advance_calculator.py
@@ -0,0 +1,17 @@
+from addition import add
+from subtraction import sub
+from division import div
+from multiplication import mul
+from square import square
+from square_root import squareroot
+from floor_division import floor
+from modulo_division import modulo
+
+print(add(x=20, y=50))
+print(sub(x=90, y=60))
+print(div(x=100, y=10))
+print(mul(x=12, y=20))
+print(square(x=60))
+print(squareroot(x=144))
+print(floor(x=160, y=10))
+print(modulo(x=190, y=10))
---
+++ modules/floor_division.py
@@ -0,0 +1,2 @@
+def floor(x:int,y:int):
+    return x//y if y!=0 else "Zero division not possible"
